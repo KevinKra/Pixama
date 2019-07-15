c25f44d1301c5427befc5c640fe72d0e9ab40efb
Kevin Krato <kratok08@gmail.com>
2019-07-15 17:05:02 -0600

    Create api folder

---
+++ src/api/apiCalls.js
@@ -0,0 +1,9 @@
+import API_KEY from "./apikey";
+
+export const fetchPopularMovies = async () => {
+  let response = await fetch(
+    `https://api.themoviedb.org/3/discover/movie?certification_country=US&api_key=${API_KEY}`
+  );
+  const movies = await response.json();
+  console.log(movies);
+};
