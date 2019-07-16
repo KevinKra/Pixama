22c3cc9eb7454174153d8b9fae63e6ad029af11b
Kevin Krato <kratok08@gmail.com>
2019-07-16 07:36:28 -0600

    Convert to class component to allow self contained transitions

--- src/components/HeroImage/HeroImage.js
+++ src/components/HeroImage/HeroImage.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import "./HeroImage.scss";
 
-export default function HeroImage() {
-  return (
-    <section className="HeroImage">
-      <div className="hero-image" />
-      <div className="screen" />
-      <article className="hero-text">Very neat</article>
-    </section>
-  );
+export default class HeroImage extends Component {
+  state = {
+    content: [
+      {
+        img:
+          "https://images.unsplash.com/photo-1547191516-45e9e58822db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+        header: "A Wonderous Voyage",
+        body: "Across the Sea."
+      }
+    ]
+  };
+  render() {
+    return (
+      <section className="HeroImage">
+        <div
+          className="hero-image"
+          style={{ backgroundImage: `url(${this.state.content[0].img})` }}
+        />
+        <div className="screen" />
+        <article className="hero-text">
+          <h2>{this.state.content[0].header}</h2>
+          <p>{this.state.content[0].body}</p>
+        </article>
+      </section>
+    );
+  }
 }
