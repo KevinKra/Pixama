a4a33b88bab739502088622dada31eaa4ecff1d4
Kevin Krato <kratok08@gmail.com>
2019-07-16 08:03:15 -0600

    Convert to Class component and pass in dummy state data

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
+import * as apiCalls from "../../api/apiCalls";
 import "./MainPage.scss";
 import Carousel from "../Carousel/Carousel";
 import HeroImage from "../HeroImage/HeroImage";
 
-export default function MainPage() {
-  return (
-    <section className="MainPage">
-      <HeroImage />
-      <section className="main-body">
-        <Carousel />
-        <article className="Carousel">
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-          <article className="MovieCard">MovieCard</article>
-        </article>
-        <article className="Carousel">carousel</article>
-        <article className="Carousel">carousel</article>
+export default class MainPage extends Component {
+  //movies state will be updated by the fetched result from the API call,
+  //the content will be mapped through with the carousels
+  // --- Once redux is introduced we will extract the local state and use Rprops
+  state = {
+    movies: [
+      {
+        carouselName: "Action",
+        movies: [{ title: "StarWars", body: "Swish-Swoosh" }]
+      },
+      {
+        carouselName: "Comedy",
+        movies: [{ title: "FunnyFun", body: "Good-Laugh" }]
+      }
+    ]
+  };
+  componentDidMount() {
+    apiCalls.fetchPopularMovies();
+  }
+  render() {
+    return (
+      <section className="MainPage">
+        <HeroImage />
+        <section className="main-body">
+          <Carousel />
+          <Carousel />
+          <Carousel />
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
 }
