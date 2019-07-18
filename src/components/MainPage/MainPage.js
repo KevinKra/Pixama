e42a865126d50cb6e3e906f6185a74f4b4baed8f
Kevin Krato <kratok08@gmail.com>
2019-07-18 16:36:05 -0600

    Remove redundant code

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,34 +1,15 @@
 import React, { Component } from "react";
-import * as apiCalls from "../../api/apiCalls";
 import "./MainPage.scss";
 import Carousel from "../Carousel/Carousel";
 import HeroImage from "../HeroImage/HeroImage";
 
-
 export default class MainPage extends Component {
-  //movies state will be updated by the fetched result from the API call,
-  //the content will be mapped through with the carousels
-  // --- Once redux is introduced we will extract the local state and use Rprop
-  state = {
-    movies: []
-  };
-
-  async componentDidMount() {
-    await this.fetchData();
-  }
-
-  fetchData = async () => {
-    const films = await apiCalls.fetchPopularMovies();
-    console.log(films);
-    this.setState({ movies: films });
-  };
-
   render() {
     return (
       <section className="MainPage">
         <HeroImage />
         <section className="main-body">
-          <Carousel title="Popular Movies" movies={this.state.movies} />
+          <Carousel title="Popular Movies" movies={this.props.movies} />
         </section>
       </section>
     );
