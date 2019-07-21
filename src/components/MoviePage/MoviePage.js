c9cc0f583ed18ff5610e00c803ed9cd375206572
Kevin Krato <kratok08@gmail.com>
2019-07-21 14:24:09 -0600

    Scaffold and bring in data from fetch

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import * as apiCalls from "../../api/apiCalls";
 
-export default function MoviePage() {
-  return (
-    <div>
-      <p>MoviePage</p>
-    </div>
-  );
+export default class MoviePage extends Component {
+  state = {
+    movie: {}
+  };
+  async componentDidMount() {
+    const movie = await apiCalls.fetchFightClub();
+    this.setState({ movie });
+  }
+  render() {
+    return <div>MoviePage</div>;
+  }
 }
