107dd8d10b530cc12a55f025100e86d5ff9a0661
Kevin Krato <kratok08@gmail.com>
2019-07-21 15:50:14 -0600

    Build base component

--- src/components/MoviePage/MoviePage.js
+++ src/components/MoviePage/MoviePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import * as apiCalls from "../../api/apiCalls";
+import "./MoviePage.scss";
 
 export default class MoviePage extends Component {
   state = {
@@ -9,7 +10,71 @@ export default class MoviePage extends Component {
     const movie = await apiCalls.fetchFightClub();
     this.setState({ movie });
   }
+
+  divStyle = (image, position) => {
+    return {
+      backgroundImage: `url(${image})`,
+      backgroundPosition: position,
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat"
+    };
+  };
   render() {
-    return <div>MoviePage</div>;
+    const {
+      poster_path,
+      overview,
+      original_title,
+      original_language,
+      release_date,
+      runtime,
+      status,
+      revenue,
+      homepage,
+      tagline,
+      vote_average,
+      production_companies
+    } = this.state.movie;
+    return (
+      <section className="MoviePage">
+        <section className="mp-primary-content">
+          <div
+            className="poster-image"
+            style={this.divStyle(
+              `https://image.tmdb.org/t/p/original${poster_path}`,
+              "center top"
+            )}
+          />
+          <aside>
+            <div>
+              <h1>{original_title}</h1>
+              <h3>{tagline}</h3>
+              <p>favorited</p>
+              <p>{overview}</p>
+              <p>{status}</p>
+              <p>length {runtime}</p>
+              <p>Released: {release_date}</p>
+              <p>{revenue}</p>
+              <p>{vote_average}</p>
+              <p>{original_language}</p>
+              <a href={homepage} />
+            </div>
+            <section className="production-companies">
+              {production_companies &&
+                production_companies.map(
+                  company =>
+                    company.logo_path && (
+                      <img
+                        src={`https://image.tmdb.org/t/p/w45${
+                          company.logo_path
+                        }`}
+                        alt={company.name}
+                      />
+                    )
+                )}
+            </section>
+          </aside>
+        </section>
+      </section>
+    );
   }
 }
