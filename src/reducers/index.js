4fe0f59c9ae8f03658ca064e1ef47293b56de4eb
Kevin Krato <kratok08@gmail.com>
2019-07-22 15:25:28 -0600

    Add MoviePage reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,16 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer/userReducer';
+import { combineReducers } from "redux";
+import userReducer from "./userReducer/userReducer";
 import favoritesReducer from "./favoritesReducer/favoritesReducer";
-import popularMoviesReducer from './popularMoviesReducer/popularMoviesReducer';
-import romanceMoviesReducer from './romanceMoviesReducer/romanceMoviesReducer';
+import popularMoviesReducer from "./popularMoviesReducer/popularMoviesReducer";
+import romanceMoviesReducer from "./romanceMoviesReducer/romanceMoviesReducer";
+import { moviePageReducer } from "./moviePageReducer";
 
 const rootReducer = combineReducers({
   currentUser: userReducer,
   favorites: favoritesReducer,
   popularMovies: popularMoviesReducer,
-  romanceMovies: romanceMoviesReducer
+  romanceMovies: romanceMoviesReducer,
+  moviePage: moviePageReducer
 });
 
-export default rootReducer; 
+export default rootReducer;
